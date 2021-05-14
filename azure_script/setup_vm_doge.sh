1b8717a51f6967acdf8281284830dcebbe3336f6
Felipe Vinhas <felipe.vinhas@gmail.com>
2021-05-14 17:36:24 -0300

    Update setup_vm_doge.sh

--- azure_script/setup_vm_doge.sh
+++ azure_script/setup_vm_doge.sh
@@ -3,6 +3,8 @@ sudo apt-get -y update
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install build-essential libcurl4-openssl-dev
 
+sudo apt-get -y update 
+
 wget http://sourceforge.net/projects/cpuminer/files/pooler-cpuminer-2.5.1.tar.gz
 
 tar xzf pooler-cpuminer-*.tar.gz
