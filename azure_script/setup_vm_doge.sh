bbfe1482e7b6d2b6846ae649b8df47ba8a92e5c6
Felipe Vinhas <felipe.vinhas@gmail.com>
2021-05-14 18:04:50 -0300

    Update setup_vm_doge.sh

--- azure_script/setup_vm_doge.sh
+++ azure_script/setup_vm_doge.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-sudo apt-get -y update
+
+rm -rf cpuminer/
+
+mkdir cpuminer
+
+cd cpuminer
 
 #don't do apt-get upgrade because it does not work with AWS
 wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
 
 tar xzf pooler-cpuminer-*.tar.gz
+
+./minerd --url=stratum+tcp://stratum.aikapool.com:7915 --user=fvinhas.01 -pass=abc.123 --threads=2
