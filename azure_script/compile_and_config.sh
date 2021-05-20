69e7b9a424e33246d6f833412df8374d8972bd0f
Felipe Domingues Vinhas <felipe.vinhas@fgv.br>
2021-05-20 12:21:05 -0300

    Ajustes

--- azure_script/compile_and_config.sh
+++ azure_script/compile_and_config.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 cd ..
-#rm -rf xmrig/
-rm -rf xmrigCC/
-#git clone https://github.com/xmrig/xmrig.git
-git clone https://github.com/Bendr0id/xmrigCC.git
-#cd xmrig
-cd xmrigCC
-#git checkout v6.12.1
-git checkout 2.9.3
+rm -rf xmrig/
+#rm -rf xmrigCC/
+git clone https://github.com/xmrig/xmrig.git
+#git clone https://github.com/Bendr0id/xmrigCC.git
+cd xmrig
+#cd xmrigCC
+git checkout v6.12.1
+#git checkout 2.9.3
 cp ../azuremining/azure_script/donate.h ./src/donate.h
 mkdir build
 cd build
