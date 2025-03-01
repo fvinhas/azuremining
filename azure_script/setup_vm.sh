329bf6567081078f7d525d72418af6a87b5acc3b
Felipe Vinhas <felipe.vinhas@gmail.com>
2025-03-01 15:31:16 -0300

    Update setup_vm.sh

--- azure_script/setup_vm.sh
+++ azure_script/setup_vm.sh
@@ -3,7 +3,7 @@ sudo apt-get -y update
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 
-sudo sysctl -w vm.nr_hugepages=1400
+sudo sysctl -w vm.nr_hugepages=1250
 git clone https://github.com/fvinhas/azuremining
 cd azuremining
 azure_script/compile_and_config.sh
